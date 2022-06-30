ae7d48b683f82e64a8010493bbf996b6d2f58a15
My1602 <yilkalhailemichael@gmail.com>
2022-06-30 14:46:14 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+*main - prints the size of various types
+*Return: 0 if exited properly, n0n-zero otherwise
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
