98c638d7eaebf930ac608cccfd34996340f65448
My1602 <yilkalhailemichael@gmail.com>
2022-06-30 14:56:32 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - print the size of various types 
+* Return: 0 if exited properly, non-zer0 otherwise
+*/
+init main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art.\");
+return (0);
+}
